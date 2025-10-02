e9d2c4d4d79a564f1ce6372606803017352e562f
Alfiq123 <fauzitaufiqurrahman218@gmail.com>
2025-10-02 21:38:04 +0700

    📦 Deploy 1.0.3

---
+++ Private/BroCode/7. Type Conversion.cpp
@@ -0,0 +1,3 @@
+//
+// Created by algorithm on 10/2/25.
+//
---
+++ Private/BroCode/9. Math Functions.cpp
@@ -0,0 +1,26 @@
+//
+// Created by algorithm on 10/2/25.
+//
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+    // double x = 3.14;
+    // double y = 5;
+    // double z;
+
+    // z = std::max(x, y);
+    // z = std::min(x, y);
+    // z = pow(2, 4);
+    // z = sqrt(9);
+    // z = abs(-16);
+    // z = round(x);
+    // z = ceil(x);
+    // z = floor(x);
+
+    // std::cout << z;
+
+    return 0;
+}
